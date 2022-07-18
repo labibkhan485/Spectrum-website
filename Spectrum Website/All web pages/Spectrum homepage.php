96d965de7c207b819b94c4b1dea4c9dd8f460e96
Labib <amirlabibkhan02@gmail.com>
2022-07-18 19:00:08 +0600

    Website updated

--- Spectrum Website/All web pages/Spectrum homepage.php
+++ Spectrum Website/All web pages/Spectrum homepage.php
@@ -22,12 +22,12 @@
         <img class="spectrum_logo" src="../All pictures/spectrum logo.png">
         <p class="Spectrum">SPECTRUM</p>
         <ul style="list-style: none;">
-          <li><a href="http://localhost/Spectrum%20Website/All%20web%20pages/Spectrum%20homepage.php">Home</a></li>
-          <li><a href="https://www.w3schools.com/html/html_links.asp#">About</a></li>
-          <li><a href="http://localhost/Spectrum%20Website/All%20web%20pages/Events.php">Events</a></li>
-          <li><a href="http://localhost/Spectrum%20Website/All%20web%20pages/BlogPage.php">Blog</a></li>
-          <li><a href="#">Team</a></li>
-          <li><a href="http://localhost/Spectrum%20Website/All%20web%20pages/Contact%20page.php">Contact</a></li>
+          <li><a href="http://localhost/Spectrum%20Website/All%20web%20pages/Spectrum%20homepage.php"><i class="fa fa-home"></i>Home</a></li>
+          <li><a href="https://www.w3schools.com/html/html_links.asp#"><i class="fa fa-clone"></i>About</a></li>
+          <li><a href="http://localhost/Spectrum%20Website/All%20web%20pages/Events.php"><i class="fa fa-calendar-o"></i>Events</a></li>
+          <li><a href="http://localhost/Spectrum%20Website/All%20web%20pages/BlogPage.php"><i class="fa fa-comment-o"></i>Blog</a></li>
+          <li><a href="http://localhost/Spectrum%20Website/All%20web%20pages/Team.php#"><i class="fa fa-users"></i>Team</a></li>
+          <li><a href="http://localhost/Spectrum%20Website/All%20web%20pages/Contact%20page.php"><i class="fa fa-phone"></i>Contact</a></li>
         </ul>
     </div>
     <div class="welcomehead">
---
+++ Spectrum Website/All web pages/Team.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Spectrum Team</title>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="../TeamPage.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <div class="homepage_header">
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+          <i class="fa fa-bars"></i>
+        </label>
+        
+        <img class="spectrum_logo" src="../All pictures/spectrum logo.png">
+        <p class="Spectrum">SPECTRUM</p>
+        <ul style="list-style: none;">
+          <li><a href="http://localhost/Spectrum%20Website/All%20web%20pages/Spectrum%20homepage.php"><i class="fa fa-home"></i>Home</a></li>
+          <li><a href="https://www.w3schools.com/html/html_links.asp#"><i class="fa fa-clone"></i>About</a></li>
+          <li><a href="http://localhost/Spectrum%20Website/All%20web%20pages/Events.php"><i class="fa fa-calendar-o"></i>Events</a></li>
+          <li><a href="http://localhost/Spectrum%20Website/All%20web%20pages/BlogPage.php"><i class="fa fa-comment-o"></i>Blog</a></li>
+          <li><a href="#"><i class="fa fa-users"></i>Team</a></li>
+          <li><a href="http://localhost/Spectrum%20Website/All%20web%20pages/Contact%20page.php"><i class="fa fa-phone"></i>Contact</a></li>
+        </ul>
+    </div>
+    
+    <div class="login">
+      <p><a style="color:black;" href="http://localhost/Spectrum%20Website/All%20web%20pages/LogIn.php">Log In</a></p>
+    </div>
+    
+  </body>
+</html>
