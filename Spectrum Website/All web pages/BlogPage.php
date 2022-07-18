2f10490f322cb4cfbea584f4e5373d4a6554376a
Labib <amirlabibkhan02@gmail.com>
2022-07-18 06:26:13 +0600

    Website updated

---
+++ Spectrum Website/All web pages/BlogPage.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>BlogPage</title>
+  </head>
+</html>
---
+++ Spectrum Website/All web pages/Spectrum homepage.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Spectrum</title>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="/Spectrum Website/Homepage.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <div class="homepage_header">
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+          <i class="fa fa-bars"></i>
+        </label>
+        
+        <img class="spectrum_logo" src="../All pictures/spectrum logo.png">
+        <p class="Spectrum">SPECTRUM</p>
+        <ul style="list-style: none;">
+          <li><a href="http://localhost/Spectrum%20Website/All%20web%20pages/Spectrum%20homepage.php">Home</a></li>
+          <li><a href="https://www.w3schools.com/html/html_links.asp#">About</a></li>
+          <li><a href="http://localhost/Spectrum%20Website/All%20web%20pages/Events.php">Events</a></li>
+          <li><a href="http://localhost/Spectrum%20Website/All%20web%20pages/BlogPage.php">Blog</a></li>
+          <li><a href="#">Team</a></li>
+          <li><a href="http://localhost/Spectrum%20Website/All%20web%20pages/Contact%20page.php">Contact</a></li>
+        </ul>
+    </div>
+    <div class="welcomehead">
+      <p>Welcome to SPECTRUM</p>
+    </div>
+    <div class="tagline">
+      <p>Skill development club of KUET</p>
+    </div>
+  </body>
+</html>
